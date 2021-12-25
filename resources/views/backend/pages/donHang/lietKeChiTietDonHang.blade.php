5f5d703da10a0e09de2e25e871ada8d7836d5cb0
Hoang Huy <githoanghuy@gmail.com>
2021-12-25 18:43:27 +0700

    Thong tin chi tiet don hang

--- resources/views/backend/pages/donHang/lietKeChiTietDonHang.blade.php
+++ resources/views/backend/pages/donHang/lietKeChiTietDonHang.blade.php
@@ -1,34 +1,82 @@
 @extends('backend.layouts.master')
 @section('title','Liệt kê chi tiết đơn hàng')
 @section('content')
+
+<!DOCTYPE html>
+<html>
+<style>
+table, th, td {
+  border:groove;
+}
+</style>
 <div class="container-fluid" id="container-wrapper">
     <div class="col-lg-12">
         <div class="card mb-4">
+             <h1 style="text-align: center;"><b>THÔNG TIN CHI TIẾT ĐƠN HÀNG</b>  </h1>
             <div class="card-header d-flex flex-row align-items-center justify-content-between">
-                <h6 class="m-0 font-weight-bold text-primary">Thông tin khách hàng</h6>
+                <h6 class="m-0 font-weight-bold text-primary" style="text-aligin=center">THÔNG TIN ĐƠN HÀNG</h6>
+                <h6 class="m-0 font-weight-bold text-primary"><p>NGÀY ĐẶT: {{$order_by_id->ngayDatHang}}</p></td></h6>
             </div>
-            <hr>
-            <div class="cus_order" style="padding-left: 15px">
-                <p>Tên khách hàng: {{$order_by_id->tenNguoiNhanHang}}</p>
-                <p>Địa chỉ nhận hàng: {{$order_by_id->diaChiGiaoHang}}</p>
-                <p>Số điện thoại: {{$order_by_id->soDienThoai}}</p>
-                <p>Ngày đặt hàng: {{$order_by_id->ngayDatHang}}</p>
-                <p>
-                    Trạng thái đơn hàng:
+            
+            <table style="width:100%;">
+            <tr>
+                <th class="m-0 font-weight-bold text-primary" style="text-align: center;" >Thông tin khách hàng</th>
+                <th class="m-0 font-weight-bold text-primary" style="text-align: center;">Địa chỉ nhận hàng</th>
+                <th class="m-0 font-weight-bold text-primary" style="text-align: center;">Trạng thái đơn hàng</th>
+            </tr>
+            <br>
+            <tr>
+                <td><b style="color:red;">Tên khách hàng:</b>  {{$order_by_id->tenNguoiNhanHang}}</Tên>
+                <p><b style="color:red;"> Số điện thoại: </b>{{$order_by_id->soDienThoai}}</p>
+                <td style="text-align: center;"><p>{{$order_by_id->diaChiGiaoHang}}</p></td>
+                <td style="text-align: center;"><p>
                     @if($order_by_id->trangThaiDonHang == 0) Đơn hàng mới
                     @elseif($order_by_id->trangThaiDonHang == 1) Đang giao hàng
                     @elseif($order_by_id->trangThaiDonHang == 2) Đã giao hàng
                     @else Đã hủy @endif
-                </p>
-            </div>
+                </p></td>
+            </tr>
+            </table>
+            <tfoot>
+                        <tr>
+                            <td colspan=2>
+                                <form action="{{URL::to('/update-status-order')}}" method="post" role="form">
+                                {!! csrf_field() !!}
+                                    <input type="hidden" name="id_order" value="{{$order_by_id->maDonHang}}">
+                                    <div class="form-group" data-select2-id="11" style="margin-top:10px" >
+                                        <label for="select2SinglePlaceholder" class="m-0 font-weight-bold text-primary" style="display: block;">Trạng thái đơn hàng</label>
+                                        <select
+                                            class="select2-single-placeholder form-control select2-hidden-accessible"
+                                            name="state_order" id="select2SinglePlaceholder"
+                                            data-select2-id="select2SinglePlaceholder" tabindex="-1"
+                                            style="width: 10%; display: inline-table;" aria-hidden="true">
+                                            <option value="0" @if($order_by_id->trangThaiDonHang == 0) selected @endif >Đơn hàng mới</option>
+                                            <option value="1" @if($order_by_id->trangThaiDonHang == 1) selected @endif>Đang giao hàng</option>
+                                            <option value="2" @if($order_by_id->trangThaiDonHang == 2) selected @endif>Đã giao hàng</option>
+                                            <option value="3" @if($order_by_id->trangThaiDonHang == 3) selected @endif >Hủy đơn hàng</option>
+                                        </select>
+                                        <button type="submit" class="btn btn-primary" style="display: inline-table;">Cập
+                                            nhật</button>
+                                    </div>
+                                </form>
+                           
+                                
 
-        </div>
-    </div>
+                            </td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
 
-    <div class="col-lg-12">
+                    </tfoot>
+                    <br>
+            <hr>
+            <div class="cus_order" style="padding-left: 15px">
+                
+                <div class="col-lg-12">
         <div class="card mb-4">
             <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                <h6 class="m-0 font-weight-bold text-primary">Thông tin chi tiết đơn hàng</h6>
+                <h6 class="m-0 font-weight-bold text-primary">Thông tin đơn hàng</h6>
             </div>
             <div class="table-responsive p-3">
                 <table class="table align-items-center table-flush table-hover" id="dataTableHover">
@@ -38,7 +86,7 @@
                             <th>Tên sản phẩm</th>
                             <th>Giá</th>
                             <th>Số lượng</th>
-                            <th>Thành tiền</th>
+                            <th>Tổng tiền</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -68,45 +116,21 @@
                                 </span> VNĐ</td>
                         </tr>
                         @endforeach
-                    </tbody>
-                    <tfoot>
                         <tr>
-                            <td>Tổng cộng: </td>
+                            <td class="m-0 font-weight-bold text-primary">Tổng thành tiền </td>
                             <td colspan=4>@php echo number_format($tongTien) @endphp VNĐ</td>
                         </tr>
-                        <tr>
-                            <td colspan=2>
-                                <form action="{{URL::to('/update-status-order')}}" method="post" role="form">
-                                {!! csrf_field() !!}
-                                    <input type="hidden" name="id_order" value="{{$order_by_id->maDonHang}}">
-                                    <div class="form-group" data-select2-id="11" style="margin-top:10px">
-                                        <label for="select2SinglePlaceholder" style="display: block;">Trạng thái</label>
-                                        <select
-                                            class="select2-single-placeholder form-control select2-hidden-accessible"
-                                            name="state_order" id="select2SinglePlaceholder"
-                                            data-select2-id="select2SinglePlaceholder" tabindex="-1"
-                                            style="width: 30%; display: inline-table;" aria-hidden="true">
-                                            <option value="0" @if($order_by_id->trangThaiDonHang == 0) selected @endif >Đơn hàng mới</option>
-                                            <option value="1" @if($order_by_id->trangThaiDonHang == 1) selected @endif>Đang giao hàng</option>
-                                            <option value="2" @if($order_by_id->trangThaiDonHang == 2) selected @endif>Đã giao hàng</option>
-                                            <option value="3" @if($order_by_id->trangThaiDonHang == 3) selected @endif >Hủy đơn hàng</option>
-                                        </select>
-                                        <button type="submit" class="btn btn-primary" style="display: inline-table;">Cập
-                                            nhật</button>
-                                    </div>
-                                </form>
-
-                            </td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-
-                    </tfoot>
+                    </tbody>
                 </table>
             </div>
         </div>
     </div>
+            </div>
+
+        </div>
+    </div>
+
+    
 </div>
 
 @endsection
