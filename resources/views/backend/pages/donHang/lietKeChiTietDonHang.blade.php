6c7c18eb192b1b7f3b55af20ddcbd47788a186b6
Hoang Huy <githoanghuy@gmail.com>
2022-01-09 13:49:16 +0700

    Liet Ke Don Hang - Giao Dien

--- resources/views/backend/pages/donHang/lietKeChiTietDonHang.blade.php
+++ resources/views/backend/pages/donHang/lietKeChiTietDonHang.blade.php
@@ -8,7 +8,7 @@
 table,
 th,
 td {
-    border: groove;
+    border: 0;
 }
 </style>
 <div class="container-fluid" id="container-wrapper">
@@ -19,50 +19,17 @@ td {
                 <!-- <h3 style="text-align: center;">THÔNG TIN CHI TIẾT ĐƠN HÀNG</h3> -->
                 <div class="card-header d-flex flex-row align-items-center justify-content-between">
                     <h6 class="m-0 font-weight-bold text-primary" style="text-aligin=center">THÔNG TIN CHI TIẾT ĐƠN HÀNG
-                    </h6>
-                    <h6 class="m-0 font-weight-bold text-primary">
-                        NGÀY ĐẶT: {{$order_by_id->ngayDatHang}}
-                    </h6>
-                </div>
-                <div class="p-3">
-                    <table style="width:100%;" class="table align-items-center table-flush table-hover "
-                        id="dataTableHover">
-                        <tr>
-                            <th class="m-0 font-weight-bold text-primary" style="text-align: center;">Thông tin khách
-                                hàng</th>
-                            <th class="m-0 font-weight-bold text-primary" style="text-align: center;">Địa chỉ nhận hàng
-                            </th>
-                            <th class="m-0 font-weight-bold text-primary" style="text-align: center;">Trạng thái đơn
-                                hàng</th>
-                        </tr>
-                        <br>
-                        <tr>
-                            <td><b style="color:red;">Tên khách hàng:</b> {{$order_by_id->tenNguoiNhanHang}}</Tên>
-                                <p><b style="color:red;"> Số điện thoại: </b>{{$order_by_id->soDienThoai}}</p>
-                            <td style="text-align: center;">
-                                <p>{{$order_by_id->diaChiGiaoHang}}</p>
-                            </td>
-                            <td style="text-align: center;">
-                                <p>
-                                    @if($order_by_id->trangThaiDonHang == 0) Đơn hàng mới
-                                    @elseif($order_by_id->trangThaiDonHang == 1) Đang giao hàng
-                                    @elseif($order_by_id->trangThaiDonHang == 2) Đã giao hàng
-                                    @else Đã hủy @endif
-                                </p>
-                            </td>
-                        </tr>
-                    </table>
-
+                    </h6>   
                     <form action="{{URL::to('/update-status-order')}}" method="post" role="form">
                         {!! csrf_field() !!}
                         <input type="hidden" name="id_order" value="{{$order_by_id->maDonHang}}">
-                        <div class="form-group" data-select2-id="11" style="margin-top:20px">
+                        <div class="form-group" data-select2-id="11" style="margin-left:60px;">
                             <label for="select2SinglePlaceholder" class="m-0 font-weight-bold text-primary"
-                                style="display: block;">Trạng thái đơn hàng</label>
+                                >Trạng thái đơn hàng</label>
                             <select class="select2-single-placeholder form-control select2-hidden-accessible"
                                 name="state_order" id="select2SinglePlaceholder"
                                 data-select2-id="select2SinglePlaceholder" tabindex="-1"
-                                style="width: 15%; display: inline-table; margin-top:10px" aria-hidden="true"; >
+                                style="width: 40%; display: inline-table;" aria-hidden="true"; >
                                 <option value="0" @if($order_by_id->trangThaiDonHang == 0) selected @endif >Đơn hàng
                                     mới</option>
                                 <option value="1" @if($order_by_id->trangThaiDonHang == 1) selected @endif>Đang giao
@@ -77,68 +44,92 @@ td {
                         </div>
                     </form>
                 </div>
-            </div>
-        </div>
-    </div>
-    <div class="cus_order" style="padding-left: 15px">
-
-        <div class="col-lg-12">
-            <div class="card mb-4">
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Thông tin đơn hàng</h6>
-                </div>
-                <div class="table-responsive p-3">
-                    <table class="table align-items-center table-flush table-hover" id="dataTableHover">
-                        <thead class="thead-light">
-                            <tr>
-                                <th>STT</th>
-                                <th>Tên sản phẩm</th>
-                                <th>Giá</th>
-                                <th>Số lượng</th>
-                                <th>Tổng tiền</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @php $i = 0; $tongTien = 0 @endphp
-                            @foreach($order_detail as $key => $order)
-                            @php $i++ @endphp
-                            <tr>
-                                </td>
-                                <td>{{$i}}</td>
-                                <td><span class="text-ellipsis">{{$order->tensanpham}}</span></td>
-                                <td><span class="text-ellipsis">{{$order->giaSanPham}}</span></td>
-                                <td>
-                                    <span class="text-ellipsis">{{$order->soLuong}}</span>
-
-                                    <input type="hidden" name="order_id" class="order_code"
-                                        value="{{$order->maDonHang}}">
-                                    <input type="hidden" name="order_product_id" class="order_product_id"
-                                        value="{{$order->maSanPham}}">
-                                </td>
-                                <td><span class="text-ellipsis">
-                                        @php
-                                        $gia = $order->giaSanPham;
-                                        $sl = $order->soLuong;
-                                        $sum = $gia * $sl;
-                                        $tongTien += $sum;
-                                        echo number_format($sum);
-                                        @endphp
-                                    </span> VNĐ</td>
-                            </tr>
-                            @endforeach
-                            <tr>
-                                <td class="m-0 font-weight-bold text-primary">Tổng thành tiền </td>
-                                <td colspan=4>@php echo number_format($tongTien) @endphp VNĐ</td>
-                            </tr>
-                        </tbody>
+                <table class="table align-items-center table-flush table-hover" id="dataTableHover">
+                <thead class="thead-light">
+                       
+                        <tr>
+                            <th class="m-0 font-weight-bold text-primary" style="text-align: center;">Thông tin khách
+                                hàng</th>
+                            <th class="m-0 font-weight-bold text-primary" style="text-align: center;">Địa chỉ nhận hàng
+                            </th>
+                        </tr>
+                        <br>
+                        <tr>
+                            <td>
+                                <p><b style="color:red;">Tên khách hàng:</b> {{$order_by_id->tenNguoiNhanHang}}  </p>  
+                                <p><b style="color:red;"> Số điện thoại: </b>{{$order_by_id->soDienThoai}}</p>
+                                <p><b style="color:red;">Ngày đặt hàng: </b>{{$order_by_id->ngayDatHang}}</p>
+                                <p><b style="color:red;">Trạng thái đơn hàng:</b>
+                               
+                                    @if($order_by_id->trangThaiDonHang == 0) Đơn hàng mới
+                                    @elseif($order_by_id->trangThaiDonHang == 1) Đang giao hàng
+                                    @elseif($order_by_id->trangThaiDonHang == 2) Đã giao hàng
+                                    @else Đã hủy @endif
+                            </td> </b></p>
+                            <td style="text-align: center;">
+                                <p>{{$order_by_id->diaChiGiaoHang}}</p>
+                            </td>
+                        </tr>
+                        
                     </table>
-                </div>
-            </div>
+            <table class="table align-items-center table-flush table-hover" id="dataTableHover">
+            <thead class="thead-light">
+                    <tr>
+                        <th class="m-0 font-weight-bold text-primary">STT</th>
+                        <th class="m-0 font-weight-bold text-primary">Tên sản phẩm</th>
+                        <th class="m-0 font-weight-bold text-primary">Giá</th>
+                        <th class="m-0 font-weight-bold text-primary">Số lượng</th>
+                        <th class="m-0 font-weight-bold text-primary">Tổng tiền</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @php $i = 0; $tongTien = 0 @endphp
+                    @foreach($order_detail as $key => $order)
+                    @php $i++ @endphp
+                    <tr>
+                        </td>
+                        <td>{{$i}}</td>
+                        <td><span class="text-ellipsis">{{$order->tensanpham}}</span></td>
+                        <td><span class="text-ellipsis">{{$order->giaSanPham}}</span></td>
+                        <td>
+                            <span class="text-ellipsis">{{$order->soLuong}}</span>
+
+                            <input type="hidden" name="order_id" class="order_code"
+                                value="{{$order->maDonHang}}">
+                            <input type="hidden" name="order_product_id" class="order_product_id"
+                                value="{{$order->maSanPham}}">
+                        </td>
+                        <td><span class="text-ellipsis">
+                                @php
+                                $gia = $order->giaSanPham;
+                                $sl = $order->soLuong;
+                                $sum = $gia * $sl;
+                                $tongTien += $sum;
+                                echo number_format($sum);
+                                @endphp
+                            </span> VNĐ</td>
+                    </tr>
+                    @endforeach
+                    <tr>
+                        <td class="m-0 font-weight-bold text-primary">Tổng thành tiền </td>
+                        <td colspan=4>@php echo number_format($tongTien) @endphp VNĐ</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
+</div>
+</div>
+</div>
+</div>
+            </div>
+            
+        </div>
+    </div>
+    
+ 
 
-</div>
-</div>
+
 
 
 </div>
--- resources/views/backend/pages/user/lietKeUser.blade.php
+++ resources/views/backend/pages/user/lietKeUser.blade.php
@@ -42,7 +42,7 @@ hr {
                     <h6 class="m-0 font-weight-bold text-primary">Danh sách user</h6>
                 </div>
                 <div class="table-responsive">
-                    <table class="table align-items-center table-flush">
+                <table class="table align-items-center table-flush table-hover" id="dataTableHover">
                         <thead class="thead-light">
                             <tr>
                                 <th>ID</th>
